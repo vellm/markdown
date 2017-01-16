cb330a604b07c96876039e4c25eeb175c25f1917
Henrik Fricke <henrik.fricke@jimdo.com>
2017-01-16 11:36:20 +0100

    use port variable for heroku

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"os"
+
 	"github.com/HenrikFricke/markdown/api"
 	"github.com/HenrikFricke/markdown/delivery"
 	"github.com/kataras/iris"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 	iris.Get("/:site", delivery.GetSite)
 	iris.Post("/api/v1/upload", api.UploadMarkdownFile)
-	iris.Listen(":8080")
+	iris.Listen(":" + port)
 }
