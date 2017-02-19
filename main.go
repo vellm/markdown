85aef9025ac71add941bc1b99fde528653d6c48b
Henrik Fricke <henrik.fricke@jimdo.com>
2017-02-19 19:06:59 +0100

    Remove https redirection
    aws cloudfront does handle it

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"os"
 
-	"github.com/iris-contrib/middleware/secure"
 	"github.com/kataras/iris"
 	"github.com/vellm/vellm.io/api"
 	"github.com/vellm/vellm.io/delivery"
@@ -12,14 +11,6 @@ import (
 var env = os.Getenv("ENV")
 
 func main() {
-	s := secure.New(secure.Options{
-		SSLRedirect:     true,
-		SSLProxyHeaders: map[string]string{"x-forwarded-proto": "https"},
-		IsDevelopment:   env == "development",
-	})
-
-	iris.Use(s)
-
 	iris.Get("/:site", delivery.GetSite)
 	iris.Post("/api/v1/upload", api.UploadMarkdownFile)
 
