2abc4b505bc76089481dc36fb6e2b933dd77163a
Henrik Fricke <henrik.fricke@jimdo.com>
2017-02-26 14:12:34 +0100

    Remove endpoint to post new vellums
    the API will handle it now

--- main.go
+++ main.go
@@ -4,15 +4,12 @@ import (
 	"os"
 
 	"github.com/kataras/iris"
-	"github.com/vellm/vellm.io/api"
 	"github.com/vellm/vellm.io/delivery"
 )
 
 var env = os.Getenv("ENV")
 
 func main() {
-	iris.Get("/:site", delivery.GetSite)
-	iris.Post("/api/v1/upload", api.UploadMarkdownFile)
 
 	iris.Listen(":" + os.Getenv("PORT"))
 }
